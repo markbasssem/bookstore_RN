2c4a5490c3fff933928744bddd1adfc283463cdd
markbasssem <mark.bassem@thirdwayv.com>
2023-09-17 14:58:09 +0300

    added avatar icon

--- src/components/ProfileDrawer.tsx
+++ src/components/ProfileDrawer.tsx
@@ -2,11 +2,23 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import {StyleSheet, View} from 'react-native';
+import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/configureStore';
 
 export default function ProfileDrawer(props) {
+  const account = useSelector((state: RootState) => state.account);
   return (
     <View style={styles.drawer}>
+      <ImageBackground
+        source={require('../../assets/profile_background.webp')}
+        style={styles.background}>
+        <Image
+          source={require('../../assets/avatar.png')}
+          style={styles.avatar}
+        />
+        <Text style={styles.username}>{account.username}</Text>
+      </ImageBackground>
       <DrawerContentScrollView>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
@@ -18,5 +30,19 @@ const styles = StyleSheet.create({
   drawer: {
     flex: 1,
     backgroundColor: '#ddd',
+  },
+  background: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+  },
+  username: {
+    color: 'white',
+    fontSize: 20,
   },
 });
