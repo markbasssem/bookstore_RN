85067cc0d5c525de7331fea11a6317ab1ba0c94e
markbasssem <mark.bassem@thirdwayv.com>
2023-10-02 17:43:19 +0300

    added buy and cart button

--- src/screens/BookDetails.tsx
+++ src/screens/BookDetails.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableOpacity } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { BookDetailsParamList } from '../types/navigation';
 import { IMAGE_URL } from './constants';
 import BackBar from '../components/BackBar';
 import Stat from '../components/Stat';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 type Props = NativeStackScreenProps<BookDetailsParamList, 'BookDetails'>;
 
 export default function BookDetails({ route, navigation }: Props): JSX.Element {
@@ -50,6 +53,14 @@ export default function BookDetails({ route, navigation }: Props): JSX.Element {
         />
         <Stat first="rating" second={route.params.book.rating} third="stars" />
         <Stat first="Length" second={route.params.book.pages} third="pages" />
+
+      </View>
+      <View style={styles.bottom}>
+        <Feather name='shopping-cart' size={30} style={styles.buyIcon} />
+        <TouchableOpacity style={styles.buy} onPress={() => { console.log("RR") }}>
+          <Text style={styles.buyText}>Buy now</Text>
+          <MaterialIcons name='arrow-forward-ios' size={28} color={"white"} />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -92,4 +103,39 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '500',
   },
+  bottom: {
+    // backgroundColor: "#ddd",
+    height: "12%",
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    right: 0,
+    left: 0,
+    // margin: 10
+    // alignSelf: "flex-end"
+  },
+  buy: {
+    backgroundColor: "#D4A056",
+    flexDirection: "row",
+    padding: "5%",
+    paddingHorizontal: "14%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    // left: "-20%",
+    borderRadius: 20,
+  },
+  buyText: {
+    fontSize: 24,
+    color: "white",
+    marginRight: "8%"
+  },
+  buyIcon: {
+    borderWidth: 1.8,
+    padding: "3%",
+    borderRadius: 17,
+    marginRight: "-4%",
+    borderColor: "#bbb"
+  }
 });
